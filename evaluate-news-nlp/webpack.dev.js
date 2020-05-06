19c5e4b22b05f6f598d03d1ef2f6118ff683c586
MohgaEmam <38896263+MohgaEmam@users.noreply.github.com>
2020-05-06 03:36:59 +0200

    Update webpack.dev.js

--- evaluate-news-nlp/webpack.dev.js
+++ evaluate-news-nlp/webpack.dev.js
@@ -5,9 +5,12 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client'
+    },
     mode: 'development',
     devtool: 'source-map',
-    stats: 'verbose',
     module: {
         rules: [
             {
@@ -27,11 +30,8 @@ module.exports = {
             filename: "./index.html",
         }),
         new CleanWebpackPlugin({
-            // Simulate the removal of files
             dry: true,
-            // Write Logs to Console
             verbose: true,
-            // Automatically remove all unused webpack assets on rebuild
             cleanStaleWebpackAssets: true,
             protectWebpackAssets: false
         })
